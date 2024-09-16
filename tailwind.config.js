1c7b496c0bda0f925a9c475ee1c2a55b73a84335
Aubin Olivrie <aubin.olivrie@smice.com>
2024-09-16 10:23:44 +0200

    Init next app & tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+}
+
