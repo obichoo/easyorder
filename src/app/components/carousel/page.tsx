3f3bd545d7ba99d911b1cdeed228e06fb89250c8
Aubin Olivrie <aubin.olivrie@smice.com>
2024-09-16 11:35:39 +0200

    add components

---
+++ src/app/components/carousel/page.tsx
@@ -0,0 +1,1 @@
+// https://www.embla-carousel.com/examples/predefined/
---
+++ src/app/components/footer/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    // prévoir un type pour la couleur (pour les boutons annuler, valider par exemple)
+    // prévoir un emplacement pour une icone à gauche du texte du bouton (pour un bouton de retour en arriere par exemple)
+    return <div>About</div>
+}
