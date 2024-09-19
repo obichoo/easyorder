b41fc47c47aea319ef5d79dad172e6334913f4cc
Elodie G <83244962+Elodie-Gr@users.noreply.github.com>
2024-09-19 16:49:20 +0200

    Update Panier suppression

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,47 +3,35 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import StripeButton from "@/app/components/stripe-button/page";
 import OrderService from "@/services/order.service";
-import OrderItemService from "@/services/order-item.service"; // Assurez-vous que ce service contient la méthode updateOrderItem
+import OrderItemService from "@/services/order-item.service";
 import getUser from '@/utils/get-user';
 
 const Cart = () => {
     const [cartItems, setCartItems] = useState<any[]>([]);
     const user = getUser();
-    
-    console.log("User ID: " + user._id);
 
     useEffect(() => {
         const fetchOrders = async () => {
             try {
                 const { data: orders } = await OrderService.getAllOrders();
-                console.log("Orders: ", orders);
-
-                // Filtrer les commandes pour l'utilisateur connecté
                 const userOrders = orders.filter(order => order.user_id === user._id);
-
-                // Pour chaque commande de l'utilisateur, récupérer les items correspondants
                 const items = await Promise.all(userOrders.map(async order => {
-                    // Pour chaque item dans une commande, récupérez les détails des produits
                     const orderItems = await Promise.all(order.items.map(async itemId => {
                         const { data: item } = await OrderItemService.getOrderItemById(itemId);
                         return item;
                     }));
 
-                    console.log("Order Items for order ", order._id, ": ", orderItems);
-
-                    // Transformer les items en un format pour le panier
                     return orderItems.map(item => ({
                         id: item._id,
                         name: item.product_id.name,
                         quantity: item.quantity,
-                        price: item.price_in_cent / 100, // Prix en euros
+                        price: item.price_in_cent / 100,
                         image: item.product_id.pictures && item.product_id.pictures.length > 0
                             ? item.product_id.pictures[0].url
                             : '/images/default-product.jpg'
                     }));
                 }));
 
-                // Aplatir le tableau et mettre à jour le state des articles du panier
                 setCartItems(items.flat());
             } catch (error) {
                 console.error("Erreur lors de la récupération des commandes :", error);
@@ -55,31 +43,39 @@ const Cart = () => {
         }
     }, [user]);
 
-    // Fonction pour mettre à jour la quantité
     const updateQuantity = async (itemId: string, newQuantity: number) => {
         try {
-            console.log("Mise à jour de l'item :", itemId, "avec la quantité :", newQuantity);
             await OrderItemService.updateOrderItem({ _id: itemId, quantity: newQuantity });
-            // Mettez à jour le state local si la mise à jour réussit
             setCartItems(prevItems =>
                 prevItems.map(item =>
                     item.id === itemId ? { ...item, quantity: newQuantity } : item
                 )
             );
+
+            // Si la quantité est mise à jour à 0, on supprime l'item
+            if (newQuantity === 0) {
+                await removeItem(itemId);
+            }
         } catch (error) {
             console.error("Erreur lors de la mise à jour de la quantité :", error);
         }
     };
-    
-
-    // Calcul du total
-    const calculateTotal = () => {
-        return cartItems.reduce((total, item) => total + item.quantity * item.price, 0);
-    };
 
     // Fonction pour supprimer un produit du panier
-    const removeItem = (id: string) => {
-        setCartItems(prevItems => prevItems.filter(item => item.id !== id));
+    const removeItem = async (id: string) => {
+        try {
+            // Supprimer l'élément de commande
+            await OrderItemService.deleteOrderItem(id);
+
+            // Mettre à jour le state local pour supprimer l'item
+            setCartItems(prevItems => prevItems.filter(item => item.id !== id));
+        } catch (error) {
+            console.error("Erreur lors de la suppression de l'item :", error);
+        }
+    };
+
+    const calculateTotal = () => {
+        return cartItems.reduce((total, item) => total + item.quantity * item.price, 0);
     };
 
     return (
@@ -95,7 +91,6 @@ const Cart = () => {
                 </div>
             ) : (
                 <div>
-                    {/* Tableau des articles du panier */}
                     <div className="rounded-2xl overflow-hidden border border-easyorder-black">
                         <table className="w-full mb-6">
                             <thead>
@@ -117,11 +112,11 @@ const Cart = () => {
                                         <td className="p-2">
                                             <input
                                                 type="number"
-                                                min="1"
+                                                min="0"
                                                 value={item.quantity}
                                                 className="border w-16 p-1 text-center"
                                                 onChange={(e) => {
-                                                    const updatedQuantity = Math.max(1, parseInt(e.target.value, 10)); // Toujours s'assurer que la quantité est au moins 1
+                                                    const updatedQuantity = Math.max(0, parseInt(e.target.value, 10)); // Toujours s'assurer que la quantité est au moins 0
                                                     if (updatedQuantity !== item.quantity) {
                                                         updateQuantity(item.id, updatedQuantity);
                                                     }
@@ -144,12 +139,10 @@ const Cart = () => {
                         </table>
                     </div>
 
-                    {/* Total */}
                     <div className="text-right">
                         <p className="text-lg font-semibold">Total : {calculateTotal().toFixed(2)} €</p>
                     </div>
 
-                    {/* Boutons */}
                     <div className="mt-6 flex justify-between">
                         <Link href="/home" className="bg-gray-200 hover:bg-gray-300 text-gray-700 py-2 px-4 rounded-md">
                             Continuer vos achats
