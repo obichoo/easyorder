84dc2dc4ba4d0f024ea72be5c517c6a4d2d33061
Elodie G <83244962+Elodie-Gr@users.noreply.github.com>
2024-09-20 17:57:23 +0200

    update panier avec mondial relay

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -10,11 +10,14 @@ import { Order } from "@/models/order.model";
 import { OrderItem } from "@/models/order-item.model";
 import { User } from "@/models/user.model";
 import { Product } from "@/models/product.model";
-import {FaArrowLeft, FaBox, FaTag, FaUser} from "react-icons/fa";
+import { FaArrowLeft } from "react-icons/fa";
+import MondialRelayWidget from "@/app/components/relaiscolis/MondialRelayWidget";
 
 const Cart = () => {
     const [cartItems, setCartItems] = useState<OrderItem[]>([]);
     const [userId, setUserId] = useState<User['_id'] | null>(null);
+    const [showPopup, setShowPopup] = useState(false);
+    const [selectedRelay, setSelectedRelay] = useState<{ street: string; postalCode: string; city: string; country: string } | null>(null);
 
     useEffect(() => {
         const userId = getUser()?._id;
@@ -25,11 +28,10 @@ const Cart = () => {
         if (userId) {
             getOrderItems();
         }
-
     }, [userId]);
 
     const getOrderItems = () => {
-        OrderService.getAllOrders().then(({ data }: { data: Order[]}) => {
+        OrderService.getAllOrders().then(({ data }: { data: Order[] }) => {
             const userOrders: Order[] = data?.filter((order: Order) => (order.user_id as User)?._id === userId && order.status === 'pending');
             const items = userOrders.map((order: Order) => order.items).flat();
             setCartItems(items as OrderItem[]);
@@ -72,6 +74,47 @@ const Cart = () => {
         }
     };
 
+    const handlePayment = () => {
+        setShowPopup(true);
+    };
+
+    const closePopup = () => {
+        setShowPopup(false);
+    };
+
+    const handleConfirm = async () => {
+        if (!selectedRelay) {
+            alert("Veuillez sélectionner un point relais avant de confirmer.");
+            return;
+        }
+    
+    
+        try {
+            // Récupère la commande actuelle
+            const { data: order } = await OrderService.getOrderById(cartItems[0]?.order_id);
+    
+            // Mets à jour l'adresse de livraison sans changer les autres détails
+            const updatedOrderData = {
+                ...order, // Conserve les autres détails de la commande
+                delivery_address: {
+                    street: selectedRelay.street,
+                    postalCode: selectedRelay.postalCode,
+                    city: selectedRelay.city,
+                    country: selectedRelay.country
+                }
+            };
+    
+            // Mets à jour la commande complète
+            await OrderService.updateOrder(updatedOrderData); // Passer l'objet mis à jour sans ID spécifique
+    
+            closePopup();
+        } catch (error) {
+            console.error("Erreur lors de la mise à jour de la commande :", error.response?.data || error.message);
+        }
+    };
+    
+    
+
     return (
         <div className="container mx-auto mt-10">
             <h1 className="text-3xl font-semibold mb-8 text-easyorder-black">Panier</h1>
@@ -88,55 +131,54 @@ const Cart = () => {
                     <div className="rounded-2xl overflow-hidden shadow-md bg-white border border-easyorder-gray mb-6">
                         <table className="w-full table-auto">
                             <thead>
-                            <tr className="bg-easyorder-green text-white">
-                                <th className="p-4 text-left">Produit</th>
-                                <th className="p-4 text-left">Quantité</th>
-                                <th className="p-4 text-left">Prix unitaire</th>
-                                <th className="p-4 text-left">Total</th>
-                                <th className="p-4 text-left">Actions</th>
-                            </tr>
+                                <tr className="bg-easyorder-green text-white">
+                                    <th className="p-4 text-left">Produit</th>
+                                    <th className="p-4 text-left">Quantité</th>
+                                    <th className="p-4 text-left">Prix unitaire</th>
+                                    <th className="p-4 text-left">Total</th>
+                                    <th className="p-4 text-left">Actions</th>
+                                </tr>
                             </thead>
                             <tbody>
-                            {cartItems.map((item: OrderItem) => (
-                                <tr key={item._id} className="border-t">
-                                    <td className="p-4 flex items-center">
-                                        <div className="grid grid-cols-[112px_500px] gap-3 h-28 mt-4">
-                                            <Link href={`/products/${(item?.product_id as Product)?._id}`} className="relative h-28 w-28">
-                                                <img
-                                                    src={(item.product_id as Product)?.pictures?.[0]?.url || 'https://via.placeholder.com/150'}
-                                                    alt={(item.product_id as Product)?.name}
-                                                    className="w-full h-full object-cover rounded-md mr-4 cursor-pointer"
-                                                />
-                                            </Link>
-
-                                            <div className="text-lg font-semibold text-easyorder-black">
-                                                <p className="flex items-center h-full">
-                                                    {(item.product_id as Product)?.name}
-                                                </p>
+                                {cartItems.map((item: OrderItem) => (
+                                    <tr key={item._id} className="border-t">
+                                        <td className="p-4 flex items-center">
+                                            <div className="grid grid-cols-[112px_500px] gap-3 h-28 mt-4">
+                                                <Link href={`/products/${(item?.product_id as Product)?._id}`} className="relative h-28 w-28">
+                                                    <img
+                                                        src={(item.product_id as Product)?.pictures?.[0]?.url || 'https://via.placeholder.com/150'}
+                                                        alt={(item.product_id as Product)?.name}
+                                                        className="w-full h-full object-cover rounded-md mr-4 cursor-pointer"
+                                                    />
+                                                </Link>
+                                                <div className="text-lg font-semibold text-easyorder-black">
+                                                    <p className="flex items-center h-full">
+                                                        {(item.product_id as Product)?.name}
+                                                    </p>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </td>
-                                    <td className="p-4">
-                                        <input
-                                            type="number"
-                                            min="0"
-                                            value={item.quantity}
-                                            className="border border-easyorder-gray rounded-md w-16 p-1 text-center focus:ring-2 focus:ring-easyorder-green"
-                                            onChange={(e) => handleQuantityChange(e.target.value, item)}
-                                        />
-                                    </td>
-                                    <td className="p-4">{((item.product_id as Product)?.price_in_cent as any) / 100} €</td>
-                                    <td className="p-4">{((item.quantity as number) * (((item.product_id as Product)?.price_in_cent as any) / 100)).toFixed(2)} €</td>
-                                    <td className="p-4">
-                                        <button
-                                            className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded-md transition-all"
-                                            onClick={() => removeItem(item._id as string)}
-                                        >
-                                            Supprimer
-                                        </button>
-                                    </td>
-                                </tr>
-                            ))}
+                                        </td>
+                                        <td className="p-4">
+                                            <input
+                                                type="number"
+                                                min="0"
+                                                value={item.quantity}
+                                                className="border border-easyorder-gray rounded-md w-16 p-1 text-center focus:ring-2 focus:ring-easyorder-green"
+                                                onChange={(e) => handleQuantityChange(e.target.value, item)}
+                                            />
+                                        </td>
+                                        <td className="p-4">{((item.product_id as Product)?.price_in_cent as any) / 100} €</td>
+                                        <td className="p-4">{((item.quantity as number) * (((item.product_id as Product)?.price_in_cent as any) / 100)).toFixed(2)} €</td>
+                                        <td className="p-4">
+                                            <button
+                                                className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded-md transition-all"
+                                                onClick={() => removeItem(item._id as string)}
+                                            >
+                                                Supprimer
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))}
                             </tbody>
                         </table>
                         <p className="text-2xl font-semibold text-easyorder-black text-right p-3">
@@ -145,13 +187,50 @@ const Cart = () => {
                     </div>
 
                     <div className="flex justify-between items-end mt-6">
-                        <Link href="/home"
-                              className="bg-easyorder-gray hover:bg-easyorder-green text-easyorder-black py-2 px-6 rounded-md transition flex items-center">
-                            <FaArrowLeft size={12} className="inline-block mr-1 -mt-[2px]"/>
+                        <Link href="/home" className="bg-easyorder-gray hover:bg-easyorder-green text-easyorder-black py-2 px-6 rounded-md transition flex items-center">
+                            <FaArrowLeft size={12} className="inline-block mr-1 -mt-[2px]" />
                             Continuer vos achats
                         </Link>
                         <div className="text-right">
-                            <StripeButton params={`?orderId=${cartItems?.[0]?.order_id}`} amount={calculateTotal() * 100}></StripeButton>
+                            <button
+                                onClick={handlePayment}
+                                className="bg-easyorder-green text-white py-2 px-6 rounded-md"
+                            >
+                                Choisir un point relais
+                            </button>
+                            
+                        </div>
+                        <StripeButton params={`?orderId=${cartItems?.[0]?.order_id}`} amount={calculateTotal() * 100}></StripeButton>
+                    </div>
+                </div>
+            )}
+
+            {showPopup && (
+                <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+                    <div className="bg-white p-5 rounded-lg shadow-lg">
+                        <h2 className="text-xl font-semibold mb-4">Sélectionnez votre point de relais</h2>
+                        <MondialRelayWidget onParcelShopSelected={(relay) => {
+                            console.log("Point relais sélectionné :", relay);
+                            setSelectedRelay(relay);
+                        }} />
+                        
+                        {selectedRelay && (
+                            <div className="mt-4">
+                                <h3 className="text-lg font-semibold">Détails du point relais sélectionné :</h3>
+                                <p><strong>Adresse :</strong> {selectedRelay.street}</p>
+                                <p><strong>Code postal :</strong> {selectedRelay.postalCode}</p>
+                                <p><strong>Ville :</strong> {selectedRelay.city}</p>
+                                <p><strong>Pays :</strong> {selectedRelay.country}</p>
+                            </div>
+                        )}
+                        
+                        <div className="flex justify-end mt-4">
+                            <button onClick={handleConfirm} className="bg-easyorder-green text-white py-2 px-4 rounded-md mr-2">
+                                Confirmer
+                            </button>
+                            <button onClick={closePopup} className="bg-red-500 text-white py-2 px-4 rounded-md">
+                                Annuler
+                            </button>
                         </div>
                     </div>
                 </div>
--- src/app/components/relaiscolis/MondialRelayWidget.tsx
+++ src/app/components/relaiscolis/MondialRelayWidget.tsx
@@ -5,7 +5,6 @@ import { useEffect } from 'react';
 const MondialRelayWidget = ({ onParcelShopSelected }) => {
     useEffect(() => {
         const loadScripts = async () => {
-            // Charger jQuery
             const jQueryScript = document.createElement('script');
             jQueryScript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js';
             jQueryScript.async = true;
@@ -14,7 +13,6 @@ const MondialRelayWidget = ({ onParcelShopSelected }) => {
             jQueryScript.onload = () => {
                 console.log("jQuery chargé");
 
-                // Charger le script du widget Mondial Relay
                 const widgetScript = document.createElement('script');
                 widgetScript.src = 'https://widget.mondialrelay.com/parcelshop-picker/jquery.plugin.mondialrelay.parcelshoppicker.min.js';
                 widgetScript.async = true;
@@ -23,16 +21,20 @@ const MondialRelayWidget = ({ onParcelShopSelected }) => {
                 widgetScript.onload = () => {
                     console.log("Widget Mondial Relay chargé");
 
-                    // Initialiser le widget
                     if (window.$) {
                         window.$("#Zone_Widget").MR_ParcelShopPicker({
                             Target: "#ParcelShopCode",
-                            Brand: "CC22ODIW", //code client mondial relay
+                            Brand: "CC22ODIW", // code client Mondial Relay
                             Country: "FR",
                             EnableGmap: true,
-                            OnSelected: (code) => {
-                                console.log("Point relais sélectionné :", code);
-                                onParcelShopSelected(code);
+                            OnParcelShopSelected: (data) => {
+                                console.log("Point relais sélectionné :", data);
+                                onParcelShopSelected({
+                                    street: data.Adresse1,
+                                    postalCode: data.CP,
+                                    city: data.Ville,
+                                    country: data.Pays
+                                });
                             },
                             OnError: (error) => {
                                 console.error("Erreur lors du chargement des points relais : ", error);
@@ -48,7 +50,6 @@ const MondialRelayWidget = ({ onParcelShopSelected }) => {
         loadScripts();
 
         return () => {
-            // Nettoyage si nécessaire
             if (window.$) {
                 window.$("#Zone_Widget").MR_ParcelShopPicker("destroy");
             }
